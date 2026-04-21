db22a1cdd97fc1dfdb7679371d8788149f5b220b
vinitcs <vinitchavan1234@gmail.com>
2026-04-21 11:42:54 +0530

    constant updated

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,4 @@
-export const API_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1";
+export const API_URL = import.meta.env.VITE_API_URL;
 
 export const PAGINATION_DEFAULTS = {
   page: 1,
